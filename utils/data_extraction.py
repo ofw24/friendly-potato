b29ef12f946b7dee323748e9033edb0294627285
sdawley1 <sdawley3@gmail.com>
2022-11-29 18:48:22 -0500

    Update data extraction

--- utils/data_extraction.py
+++ utils/data_extraction.py
@@ -12,24 +12,30 @@ def load_data(filepath: str) -> pd.DataFrame:
         usecols=["year", "state_po", "candidatevotes", "totalvotes", "party_detailed"])
     return df # no idea why the encoding is necessary
 
+def count_party_occurrences(party: str) -> float:
+    """
+    Get number of occurrences of a specific partyy
+    """
+    return sum([1 for p in df["party_detailed"] if p == party])
+
 def get_all_parties(df: pd.DataFrame) -> list:
     """
     Get all parties that people ran for Senate under from 1976-2020
+    Assumes that DataFrame is loaded from '1976-2020-senate.csv'
     """
-    parties = []
+    all_entries = []
     for party in df["party_detailed"]:
-        if party == party: # get rid of NaNs
-            # if party not in parties: parties.append(party)
-            parties.append(party)
+        if party == party: all_entries.append(party) # get rid of NaNs
+    entries_set = set(all_entries)
     if True:
         parties2 = []
-        for party in set(parties):
-            parties2.append((party, parties.count(party)))
-    return parties, parties2
+        for party in entries_set:
+            parties2.append((party, all_entries.count(party)))
+    return all_entries, parties2
 
 if __name__ == "__main__":
     filepath = "./dataverse_files/1976-2020-senate.csv"
     df = load_data(filepath)
     p, p2 = get_all_parties(df)
-    for p in sorted(p2):
-        print(f"Party {p[0]:<50s} has {p[1]:<5.0f} occurences")
+    for p in sorted(p2, key=lambda p: p[1], reverse=True):
+        print(f"{p[0]:<40s} occurred {p[1]:<5.0f} times")
