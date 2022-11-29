7c8ec7dc9dfc63d3d3ee3a56dc6520ee4059f1b8
sdawley1 <sdawley3@gmail.com>
2022-11-29 18:10:52 -0500

    Names of political parties

--- utils/data_extraction.py
+++ utils/data_extraction.py
@@ -7,9 +7,29 @@ def load_data(filepath: str) -> pd.DataFrame:
     """
     Load data into a pandas dataframe
     """
-    return pd.read_csv(filepath, header=0, encoding="latin1") # no idea why the encoding is necessary
+    df = pd.read_csv(
+        filepath, header=0, encoding="latin1", 
+        usecols=["year", "state_po", "candidatevotes", "totalvotes", "party_detailed"])
+    return df # no idea why the encoding is necessary
+
+def get_all_parties(df: pd.DataFrame) -> list:
+    """
+    Get all parties that people ran for Senate under from 1976-2020
+    """
+    parties = []
+    for party in df["party_detailed"]:
+        if party == party: # get rid of NaNs
+            # if party not in parties: parties.append(party)
+            parties.append(party)
+    if True:
+        parties2 = []
+        for party in set(parties):
+            parties2.append((party, parties.count(party)))
+    return parties, parties2
 
 if __name__ == "__main__":
-    filepath = "../dataverse_files/1976-2020-senate.csv"
+    filepath = "./dataverse_files/1976-2020-senate.csv"
     df = load_data(filepath)
-    print(df)
+    p, p2 = get_all_parties(df)
+    for p in sorted(p2):
+        print(f"Party {p[0]:<50s} has {p[1]:<5.0f} occurences")
