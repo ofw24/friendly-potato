05e39be869aa69969272bc119afa7a141bc57f65
sdawley1 <sdawley3@gmail.com>
2022-12-01 17:01:05 -0500

    Work on probability distributions

--- utils/mcmc_samplers.py
+++ utils/mcmc_samplers.py
@@ -6,116 +6,113 @@ Authors
 Sam Dawley & Oliver Wolff
 """
 import numpy as np
-from abc import ABC
+from numpy.linalg import inv, norm
+from math import factorial
+import pandas as pd
+from scipy.stats import multivariate_normal, invgamma, rv_continuous
 
-class mcmc_sampler(ABC):
+def infinisum(f: callable):
+    n, res = 0, f(0)
+    while True:
+        term = sum( f(k) for k in range(2**n,2**(n+1)) )
+        if (res+term)-res == 0:
+             break;
+        n,res = n+1, res+term
+    return res
+
+class CMP(rv_continuous):
     """
-    Base class for Markov Chain Monte Carlo sampling methods
+    Conway-Maxwell Poisson (CMP) probability distribution
     """
-    pass
-
-class gibbs_sampler(mcmc_sampler):
-    """
-    Gibbs sampling algorithm for getting posterior distributions
-    """
-    def __init__(self, 
-        starting_points: np.array, 
-        conditional_distributions: np.array, 
-        parameter_update_functions: np.array,
-        N: int, burn: int
-        ):
+    def __init__(self, loc: float, scale: float, a: int=0, b: int=np.infty):
+        if loc <= 0 or scale <= 0:
+            raise ValueError("Location and scale parameters of CMP distribution must be greater than zero.")
+        self.loc = loc
+        self.scale = scale
+    def pdf(self, y: float) -> float:
         """
-        Note that 'conditional_distributions', 'parameter_update_functions', etc. should be ordered in the same manner,
-        i.e., if conditional_distributions = [theta, gamma, sigma, ...] then parameter_update_functions = [theta, gamma, sigma, ...]
+        Probability mass function of the CMP distribution
+        Returns probability of observing y
         """
-        self.priors = starting_points
-        self.full_cond_posts = conditional_distributions
-        self.new_param_funcs = parameter_update_functions
-        self.N = N
-        self.burn = burn
-    def _update_parameters(self):
+        Q = np.sum([self.loc**j / (factorial(j) ** self.scale) for j in range(0, int(np.infty))])
+        f = (self.loc ** y) / (factorial(y) ** self.scale) / Q
+        return f / Q
+    def rvs(self, N: int=1) -> np.array:
         """
-        Get new parameters of full conditional posterior distributions after each iteration
+        Draw N random variates from the distribution
+        Algorithm for generating random variates attributed to 
+            https://rossetti.github.io/RossettiArenaBook/app-rnrv-rvs.html
+        Parameters
+        ----------
+        N (int) is the number of random variates generated
+
+        Returns
+        -------
+        instances (np.array) contains all random variates
         """
-        new_params = np.zeros(len(self.full_cond_posts))
-        for func in self.new_param_funcs:
-            new_params += func()
-
-    def sample(self, observed_data: np.array, prior_parameters: np.array):
-        """
-        Run iterations of the Gibbs sampler
-        """
-        # Create arrays for storing posterior distributions of parameters
-        posterior_arrays = np.array([])
-        for _ in self.full_cond_posts:
-            posterior_arrays = np.append(posterior_arrays, np.zeros(self.N-self.burn))
-        # Iterate
-        for t in range(self.N):
-            print(f"GIBBS SAMPLER ITERATION {t+1}")
-            # Update parameters
-            new_params = self._update_parameters()
+        instances = np.zeros(N)
+        for t in range(N):
+            instances[t] += np.sqrt( -np.log(1- np.random.rand() )  / self.theta)
+        return instances
 
 
+# # Gibbs sampling algorithm
+# def gibbs_sampler(
+#     t1_conditional: norm,
+#     t2_conditional: norm,
+#     N: int, burn: int
+#     ) -> tuple:
+#     """
+#     ...
 
+#     Parameters
+#     ----------
+#     t1_conditional (callable) full conditional posterior distribution of theta_1
+#     t2_conditional (callable) full conditional posterior distribution of theta_2
+#     N (int) is the number of iterations for the sampler
+#     burn (int) is the burn-in period
 
-
-
-# Gibbs sampling algorithm
-def gibbs_sampler(
-    t1_conditional: norm,
-    t2_conditional: norm,
-    N: int, burn: int
-    ) -> tuple:
-    """
-    ...
-
-    Parameters
-    ----------
-    t1_conditional (callable) full conditional posterior distribution of theta_1
-    t2_conditional (callable) full conditional posterior distribution of theta_2
-    N (int) is the number of iterations for the sampler
-    burn (int) is the burn-in period
-
-    Returns 
-    -------
-    t1_posterior (np.array) is the empirical posterior distribution of theta_1
-    t2_posterior (np.array) is the empirical posterior distribution of theta_2
-    """
-    # Global variables from given data
-    global a1, a2, b1, b2, sample_data, n
-    # Create arrays for storing posterior distributions of parameters
-    t1_posterior, t2_posterior = np.zeros(N-burn), np.zeros(N-burn)
-    # Create prior parameter for initialization
-    t1_prior, t2_prior = 25, 25
-    # Generate some parameters which are unchanging with each iteration
-    ensemble_sum = sum(sample_data)
-    t1_scale, t2_scale = 1 / (1/b1**2 + n), 1 / (1/b2**2 + n)
-    t1_partial_loc, t2_partial_loc = a1/b1**2 + ensemble_sum, a2/b2**2 + ensemble_sum
-    # Update parameters from full conditional distributions and append to posterior arrays
-    for t in range(N):
-        print(f"GIBBS SAMPLER ITERATION {t+1}")
-        # Generate posterior parameters and update parameters
-        # For theta_1
-        t1_loc = (t1_partial_loc - n*t2_prior) / t1_scale
-        t1_post = t1_conditional(loc=t1_loc, scale=t1_scale).rvs()
-        test = np.random.rand()
-        # test = 1
-        if test > 0.7: # Implement random updates (quasi-metropolis-hastings, if you will)
-            t1_prior = t1_post
-        # For theta_2
-        t2_loc = (t2_partial_loc - n*t1_prior) / t2_scale
-        t2_post = t2_conditional(loc=t2_loc, scale=t2_scale).rvs()
-        if test > 0.7:
-            t2_prior = t2_post
-        # If we're past the burn-in period, append realizations of the parameters to posterior arrays
-        if t >= burn:
-            t1_posterior[t-burn] += t1_post
-            t2_posterior[t-burn] += t2_post
-    return t1_posterior, t2_posterior
-
-
-
+#     Returns 
+#     -------
+#     t1_posterior (np.array) is the empirical posterior distribution of theta_1
+#     t2_posterior (np.array) is the empirical posterior distribution of theta_2
+#     """
+#     # Global variables from given data
+#     global a1, a2, b1, b2, sample_data, n
+#     # Create arrays for storing posterior distributions of parameters
+#     t1_posterior, t2_posterior = np.zeros(N-burn), np.zeros(N-burn)
+#     # Create prior parameter for initialization
+#     t1_prior, t2_prior = 25, 25
+#     # Generate some parameters which are unchanging with each iteration
+#     ensemble_sum = sum(sample_data)
+#     t1_scale, t2_scale = 1 / (1/b1**2 + n), 1 / (1/b2**2 + n)
+#     t1_partial_loc, t2_partial_loc = a1/b1**2 + ensemble_sum, a2/b2**2 + ensemble_sum
+#     # Update parameters from full conditional distributions and append to posterior arrays
+#     for t in range(N):
+#         print(f"GIBBS SAMPLER ITERATION {t+1}")
+#         # Generate posterior parameters and update parameters
+#         # For theta_1
+#         t1_loc = (t1_partial_loc - n*t2_prior) / t1_scale
+#         t1_post = t1_conditional(loc=t1_loc, scale=t1_scale).rvs()
+#         test = np.random.rand()
+#         # test = 1
+#         if test > 0.7: # Implement random updates (quasi-metropolis-hastings, if you will)
+#             t1_prior = t1_post
+#         # For theta_2
+#         t2_loc = (t2_partial_loc - n*t1_prior) / t2_scale
+#         t2_post = t2_conditional(loc=t2_loc, scale=t2_scale).rvs()
+#         if test > 0.7:
+#             t2_prior = t2_post
+#         # If we're past the burn-in period, append realizations of the parameters to posterior arrays
+#         if t >= burn:
+#             t1_posterior[t-burn] += t1_post
+#             t2_posterior[t-burn] += t2_post
+#     return t1_posterior, t2_posterior
 
 
 if __name__ == "__main__":
-    ...
+    a = CMP(0.5, 1)
+    # a.pdf(5)
+    # a = integers(blk_size=1)
+    ggg = lambda x: x+1
+    converge_sum(ggg, integers(blk_size=5))
