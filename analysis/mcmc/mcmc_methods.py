dc52be7bb3e58e1625b535a40c028dac27698b72
sdawley1 <sdawley3@gmail.com>
2022-12-20 19:10:40 -0500

    Implement Metropolis-Hastings

--- analysis/mcmc/mcmc_methods.py
+++ analysis/mcmc/mcmc_methods.py
@@ -3,12 +3,14 @@ Script for extracting and manipulating data
 """
 import numpy as np
 import pandas as pd
-from scipy.stats import invgamma, norm
+from functools import partial
+from scipy.stats import invgamma, norm ,truncnorm, uniform
 from analysis.data_manips.data_extraction import ale
+from analysis.mcmc.not_mcmc_methods import double_logistic
 
 def gibbs_sampler(data: pd.DataFrame, country: str, beta_ols: np.array, N: int, burn: int) -> np.array:
     """
-    Gibbs sampling algorithm
+    Metropolis-Hastings sampling algorithm
     Ordering for all length-6 arrays is : Delta_1, Delta_2, Delta_3, Delta_4, k, z
     """
     # Global data
@@ -22,9 +24,10 @@ def gibbs_sampler(data: pd.DataFrame, country: str, beta_ols: np.array, N: int, 
 
     # Initialize prior variances
     rate_params = np.array([15.6**2, 23.5**2, 14.5**2, 14.7**2, 3.5**2, 0.6**2])
-    variai = np.array([invgamma(a=nu0/2, scale=1/rate).rvs() for rate in rate_params]) # Variances for all of the 6 parameters
+    variai = np.array([invgamma(a=nu0/2, scale=rate).rvs() for rate in rate_params]) # Variances for all of the 6 parameters
     aS, dS = np.array([15.77, 40.97, 0.21, 19.82, 2.93, 0.40]), np.array([15.77**2, 40.97**2, (1+0.21)**2, 19.82**2, 2.93**2, (1+0.40)**2])
-    prior_param_rvs = np.array([norm(a, d).rvs() for a, d in zip(aS, dS)])
+    lbS, ubS = np.array([0, 0, 0, 0, 0, 0]), np.array([100, 100, 100, 100, 10, 1.15]) 
+    prior_param_rvs = np.array([truncnorm(a=lb, b=ub, loc=a, scale=d).rvs() for a, d, lb, ub in zip(aS, dS, lbS, ubS)])
     # prior_param_rvs = [b for b in beta_ols]
 
     # Initialize array to store results
@@ -32,24 +35,48 @@ def gibbs_sampler(data: pd.DataFrame, country: str, beta_ols: np.array, N: int, 
     
     # Start the sampling
     for t in range(N):
-        # print(f"GIBBS SAMPLER ITERATION {t+1}")
+        # print(f"METROPOLIS-HASTINGS ITERATION {t+1}")
+        u = uniform().rvs()
         # Update parameters for
         # Variai
-        var_rvs = []
-        for parameter, mean, var in zip(prior_param_rvs, aS, variai):
+        new_variai = []
+        for parameter, mean, var, prior_rate, life_exp in zip(prior_param_rvs, aS, variai, rate_params, actual_life_exps):
+            likelihood = double_logistic(prior_param_rvs, life_exp)
             tmp_var = ((nu0*var + (n-1)*sample_var) + kappa0*n*(ybar-mean)/(kappa0 + n)) / nun
-            var_rvs.append( invgamma(a=nun/2, scale=1/((nu0+n)*tmp_var/2)).rvs() )
-        variai = np.array(var_rvs)
+            proposed_point = invgamma(a=nun/2, scale=1/((nu0+n)*tmp_var/2)).rvs()
+            proposed_prob  = invgamma(a=nun/2, scale=1/((nu0+n)*tmp_var/2)).pdf(proposed_point)
+            prior_prob = invgamma(a=nu0/2, scale=1/prior_rate).pdf(var)
+            # print(prior_prob)
+            #
+            acceptance_ratio = (proposed_prob / prior_prob) / 10**3 # Scaling because of non-normalization
+            if min(np.abs(acceptance_ratio), 1) >= u:
+                new_variai.append(proposed_point)
+            else:
+                new_variai.append(var)
+        # print(f"Prior      {prior_prob}")
+        # print(f"Posterior  {proposed_prob}")
+        variai = []; variai = np.array(new_variai)
+        testing = acceptance_ratio
 
         # Delta_i's
         param_rvs = []
-        for parameter, var in zip(prior_param_rvs, variai):
-            param_rvs.append( norm(loc=parameter, scale=var).rvs() )
-        prior_param_rvs = np.array(param_rvs)
+        for parameter, var, lb, ub, a, d, life_exp in zip(prior_param_rvs, variai, lbS, ubS, aS, dS, actual_life_exps):
+            likelihood = double_logistic(prior_param_rvs, life_exp)
+            proposed_point = truncnorm(a=lb, b=ub, loc=parameter, scale=var).rvs()
+            proposed_prob  = truncnorm(a=lb, b=ub, loc=parameter, scale=var).pdf(proposed_point) * likelihood
+            prior_prob = truncnorm(a=lb, b=ub, loc=a, scale=d).pdf(parameter) 
+            #
+            acceptance_ratio = (proposed_prob / prior_prob) / 10**5 # Scaling because of non-normalization
+            if min(np.abs(acceptance_ratio), 1) >= u:
+                param_rvs.append(proposed_point)
+            else:
+                param_rvs.append(parameter)
+        prior_param_rvs = []; prior_param_rvs = np.array(param_rvs)
 
         if t > burn:
             posterior_params.append(prior_param_rvs)
 
+    print(f"{u} vs. {min(np.abs(acceptance_ratio))} vs {min(np.abs(acceptance_ratio))}")
     return np.array(posterior_params)
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -58,50 +58,50 @@ def main():
     spl = data_spline(actuals, actuals-resids, order=1) # THIS CAN NEVER BE CHANGED
     omega = uniform(0, 10).rvs()
 
-    # # GIBBS SAMPLING FOR ALL COUNTRIES
-    # all_predicted = []
-    # continents = []
-    # for count in countries:
-    #     actual = ale(df, count)
-    #     continent = np.array(rawdata["continent"][rawdata["country"] == count])[0]
-    #     print(f"{count} is in {continent}")
-    #     res = gibbs_sampler(df, count, beta_ols, 50, 10)
-    #     # Get averages for each parameter out of the gibbs sampler
-    #     gibbs_beta = np.zeros(len(res[0]))
-    #     for r in res:
-    #         for i, p in enumerate(r):
-    #             gibbs_beta[i] += p / len(res)
-    #     #
-    #     gibbs_predicted = []
-    #     for life_exp in actuals:
-    #         pred = double_logistic(gibbs_beta, life_exp) + life_exp + norm(0, (omega*spl(life_exp)**2)).rvs()
-    #         if np.absolute(pred - life_exp) / life_exp < 0.5:
-    #             gibbs_predicted.append(pred)
-    #         else:
-    #             gibbs_predicted.append( norm(loc=life_exp, scale=2**2).rvs() )
-    #         continents.append(cont_2_color(continent))
-    #     # print(gibbs_predicted)
-    #     all_predicted.append(gibbs_predicted)
-    # all_predicted = np.array(all_predicted).flatten()
+    # GIBBS SAMPLING FOR ALL COUNTRIES
+    all_predicted = []
+    continents = []
+    for count in countries:
+        actual = ale(df, count)
+        continent = np.array(rawdata["continent"][rawdata["country"] == count])[0]
+        print(f"{count} is in {continent}")
+        res = gibbs_sampler(df, count, beta_ols, 50, 10)
+        # Get averages for each parameter out of the gibbs sampler
+        gibbs_beta = np.zeros(len(res[0]))
+        for r in res:
+            for i, p in enumerate(r):
+                gibbs_beta[i] += p / len(res)
+        #
+        gibbs_predicted = []
+        for life_exp in actuals:
+            pred = double_logistic(gibbs_beta, life_exp) + life_exp + norm(0, (omega*spl(life_exp)**2)).rvs()
+            if np.absolute(pred - life_exp) / life_exp < 0.5:
+                gibbs_predicted.append(pred)
+            else:
+                gibbs_predicted.append( norm(loc=life_exp, scale=2**2).rvs() )
+            continents.append(cont_2_color(continent))
+        # print(gibbs_predicted)
+        all_predicted.append(gibbs_predicted)
+    all_predicted = np.array(all_predicted).flatten()
 
-    # # Plotting raw life expectancy gains
-    # actual_lexp = []
-    # plt.figure(figsize=(8,6))
-    # for count in countries:
-    #     # continent = np.array(rawdata["continent"][rawdata["country"] == count])[0]
-    #     le, leg = grab_life_exp_gain(df, count) # Actual life expectancy and actual gain to life expectancy
-    #     continent = get_continent(rawdata, count)
-    #     if max(np.absolute(leg)) < 5:
-    #         plt.scatter(le, leg, c=cont_2_color(continent), ec="k")
-    #     if count == "Croatia":
-    #         actual_lexp.append(le)
-    # # Superimposing gain function on top
-    # # actual_lexp = np.array(actual_lexp).flatten()
-    # # Y = double_logistic(gibbs_beta, actual_lexp)
-    # # plt.plot(actual_lexp, Y, c="k", lw=2)
-    # labels_titles(plt.gca(), xlabel="Life Expectancy", ylabel="Expected Gain")
-    # funky_legend(plt.gca())
-    # plt.show()
+    # Plotting raw life expectancy gains
+    actual_lexp = []
+    plt.figure(figsize=(8,6))
+    for count in countries:
+        # continent = np.array(rawdata["continent"][rawdata["country"] == count])[0]
+        le, leg = grab_life_exp_gain(df, count) # Actual life expectancy and actual gain to life expectancy
+        continent = get_continent(rawdata, count)
+        if max(np.absolute(leg)) < 5:
+            plt.scatter(le, leg, c=cont_2_color(continent), ec="k")
+        if count == "Croatia":
+            actual_lexp.append(le)
+    # Superimposing gain function on top
+    # actual_lexp = np.array(actual_lexp).flatten()
+    # Y = double_logistic(gibbs_beta, actual_lexp)
+    # plt.plot(actual_lexp, Y, c="k", lw=2)
+    labels_titles(plt.gca(), xlabel="Life Expectancy", ylabel="Expected Gain")
+    funky_legend(plt.gca())
+    plt.show()
 
     # Plotting
     plt.figure(figsize=(8,6))
